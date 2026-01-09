b0a0f52a77f55cd55ebaf2c8eaefbddda1a32bae
Hindhu Srimathi S K <indhusrinikil@gmail.com>
2026-01-09 14:29:03 +0530

    Time: 207 ms (11.12%) | Memory: 46 MB (5.26%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,4 +1,25 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        return list(set(permutations(nums)))
+        # return list(set(permutations(nums)))
+        
+        # using recursion 
+        res=[]
+        n=len(nums)
+        used=[False]*n
+        def dfs(path):
+            if len(path)==n:
+                res.append(path.copy())
+                return
+            for i in range(n):
+                x=nums[i]
+                if used[i]==True:
+                    continue
+                used[i]=True
+                dfs(path+[x])
+                used[i]=False
+        dfs([])
+        ans = []
+        for li in res:
+            ans.append(tuple(li))
+        return list(set(ans))
         
