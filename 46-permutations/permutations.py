46ce8c0d6174a58bdd2cd345b56f05a4fba8227b
Hindhu Srimathi S K <indhusrinikil@gmail.com>
2026-01-09 12:44:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (13.27%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,3 +1,21 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(permutations(nums))
+        # return list(permutations(nums))
+
+        # using recursion function by DFS 
+        res=[]
+        n=len(nums)
+        used=[False]*n
+        def dfs(path):
+            if len(path)==n:
+                res.append(path.copy())
+                return
+            for i in range(n):
+                x=nums[i]
+                if used[i]==True:
+                    continue
+                used[i]=True
+                dfs(path+[x])
+                used[i]=False
+        dfs([])
+        return res
